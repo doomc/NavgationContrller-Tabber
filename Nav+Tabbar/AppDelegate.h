fc93a0372506543c61a0cefb75b1fc41b6f0dbfe
XWDdoomc <714024089@qq.com>
2016-05-18 17:17:39 +0800

    Initial Commit

---
+++ Nav+Tabbar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nav+Tabbar
+//
+//  Created by 熊维东 on 16/5/18.
+//  Copyright © 2016年 熊维东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
